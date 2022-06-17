35c2a2def25bf7e7f224311303e29ba026cfc6df
Oliver <oliver721003@gmail.com>
2022-06-18 01:43:23 +0800

    139. practicing and some common gotchas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 import { Server } from '../../model/server';
 import { ServersService } from '../servers.service';
@@ -11,9 +13,17 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server!: Server;
 
-  constructor(private serversService: ServersService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private serversService: ServersService
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+
+    this.route.paramMap
+      .pipe(map((params) => +params.get('id')!))
+      .subscribe((id) => (this.server = this.serversService.getServer(id)));
   }
 }
